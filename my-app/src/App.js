00677c3eb9bee92b6207f985eea92a25d5ba7fe1
ptam1 <puitm3@gmail.com>
2020-06-19 00:02:38 -0400

    edited app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,14 @@ import './App.css';
 import NavBar from './frontend/NavBar.js';
 import HomePageSlide from './frontend/HomePageSlide.js';
 import BackgroundImage from './frontend/BackgroundImage.js';
-import './frontend/BackgroundImage.css';
-import './frontend/HomePageSlide.css';
 import Container from 'react-bootstrap/Container';
 
 class App extends Component {
     render() {
         return (
             <div className="App">               
-                <NavBar displaytext=" " />
                 <Container fluid>
+                    <NavBar displaytext=" " />
                     <HomePageSlide display />
                     <BackgroundImage display />
                 </Container>
