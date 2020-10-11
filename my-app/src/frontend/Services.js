a5f26a3619834fc995b4eb2b4c84478ea55725da
ptam1 <puitm3@gmail.com>
2020-10-11 11:24:22 -0400

    edited services description

--- my-app/src/frontend/Services.js
+++ my-app/src/frontend/Services.js
@@ -26,8 +26,8 @@ export default class Services extends Component {
                         <p className="firstParagraphProperties">
                              <br />
                              <b>    I develop websites. Each website is unqiuely tailored to your vision. If you
-                             <br /> have an existing website, I can modify or update it. I test each feature of the website, so that your website is bug free. I have over a year's experience in testing software.  
-                             <br /> I will work with you throughout the whole process from beginning to end to ensure that what I develop is what you envisioned.
+                             <br /> have an existing website, I can modify or update it. I test each feature of the website to ensure that your website is bug free. I have over a year's experience in testing software.
+                            <br /> I will work with you throughout the whole process from beginning to end to ensure that what I develop is what you envisioned.
                              <br />
                              <br />
                              </b>
@@ -38,7 +38,7 @@ export default class Services extends Component {
 
                         <p className="secondParagraphProperties">
                             <br />
-                            <b>     If you are thinking of upgrading or wanting a new fresh, modern look for your website,       I can help you with that! I can help redesign your website or help set your websit
+                            <b>     If you are thinking of upgrading or wanting a new fresh, modern look for your website,        I can help you with that! I can help redesign your website or help set up your website
                             <br />  from a creative perspective. At the end of the project, you will recieve a redesigned
                             <br />  website that you can feel confident about having and sharing. I have experience
                             <br />  working with local Atlanta businesses in redesigning their existing websites.
